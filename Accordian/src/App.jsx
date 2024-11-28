511d1e70e37938a261ad6cd345ecfd54030e1643
Sabayas <sabayasmin345@gmail.com>
2024-11-28 12:09:10 +0530

    Accordian app

---
+++ Accordian/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Accordian from './component/accordian'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="App">
+      {/* Accordian component */}
+      <Accordian></Accordian>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
